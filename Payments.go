62fe5b0afa0d2e594afd28cd1842abac5d68855c
bfrasure <bill.frasure@gmail.com>
2019-04-14 15:10:43 -0600

    +CreateProduct function

--- Payments.go
+++ Payments.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/charge"
+	"github.com/stripe/stripe-go/product"
 	"os"
 )
 
@@ -25,6 +26,26 @@ func createTestPaymentFunction(testKey string) func(string, int64) string {
 
 }
 
+func CreateProduct(testKey string) {
+	stripe.Key = os.Getenv(testKey)
+
+	params := &stripe.ProductParams{
+		Name: stripe.String("Gym Membership"),
+		Type: stripe.String(string(stripe.ProductTypeService)),
+	}
+	prod, _ := product.New(params)
+	fmt.Println("New Product Response")
+	fmt.Println(prod)
+}
+
+func CreateTestProduct() {
+	CreateProduct("TEST_STRIPE_SECRET_KEY")
+}
+
 func ExecuteTestStripePaymentWithAmount(stripePaymentToken string, amount int64) string {
 	return createTestPaymentFunction("TEST_STRIPE_SECRET_KEY")(stripePaymentToken, amount)
+}
+
+func ExecuteLiveStripePaymentWithAmount(stripePaymentToken string, amount int64) string {
+	return createTestPaymentFunction("LIVE_STRIPE_SECRET_KEY")(stripePaymentToken, amount)
 }
